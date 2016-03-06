97d83446f8820981c814767010ff3a1dd54ca824
yangli <yang.li@rea-group.com>
2016-03-06 12:31:45 +0800

    add test for image_helper.rb -yang

--- spec/common/image_helper_spec.rb
+++ spec/common/image_helper_spec.rb
@@ -4,9 +4,11 @@ describe Common::ImageHelper do
   let(:fake_image_base64) { File.read('spec/fixtures/sample_image_base64.txt') }
   let(:fake_png_path) { "spec/fixtures/icon.png" }
   let(:fake_jpg_path) { "spec/fixtures/icon.jpg" }
+  let(:fake_jpeg_path) { "spec/fixtures/icon.jpeg" }
   let(:fake_folder) { "temp_images" }
   let(:fake_small_png_path) { "#{fake_folder}/s_icon.png" }
   let(:fake_small_jpg_path) { "#{fake_folder}/s_icon.jpg" }
+  let(:fake_small_jpeg_path) { "#{fake_folder}/s_icon.jpeg" }
 
   describe "#save" do
     it "should save the image" do
@@ -28,6 +30,11 @@ describe Common::ImageHelper do
 
     it "should generate_thumbnails for jpg image" do
       image_path = subject.generate_thumbnails fake_jpg_path, fake_small_jpg_path, 0.25
+      expect(File.exist?(fake_small_jpg_path)).to eq true
+    end
+
+    it "should generate_thumbnails for jpeg image" do
+      image_path = subject.generate_thumbnails fake_jpeg_path, fake_small_jpeg_path, 0.25
       expect(File.exist?(fake_small_jpg_path)).to eq true
     end
   end
