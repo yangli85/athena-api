026384fe5b0afa62086bfd06ed509b9c9faba47f
yangli85 <yangli@CNyangli.local>
2016-03-31 20:20:10 +0800

    fix test issue -yang

--- lib/pay/wechat_pay.rb
+++ lib/pay/wechat_pay.rb
@@ -12,7 +12,7 @@ module Pay
     GATEWAY_URL = 'https://api.mch.weixin.qq.com'
     PAY_CHANNEL = "WX"
     CREATE_PREPAY_ORDER_REQUIRED_FIELDS = ['appid', 'mch_id', 'nonce_str', 'body', 'out_trade_no', 'total_fee', 'spbill_create_ip', 'notify_url', 'trade_type']
-    GENERATE_APP_PAY_REQ_REQUIRED_FIELDS =['appid', 'partnerid', 'prepay_id', 'noncestr', 'package', 'noncestr', 'timestamp']
+    GENERATE_APP_PAY_REQ_REQUIRED_FIELDS =['appid', 'partnerid', 'prepayid', 'noncestr', 'package', 'noncestr', 'timestamp']
 
     def initialize
       @app_id = ENV['WX_APP_ID']
